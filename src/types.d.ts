56fd2eaf11d7179dfcc78de01b9eaf42d6c51b6e
lpfader <lpfader@gmail.com>
2026-09-25 21:00:53 +0000

    confirmed working from front-end

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,3 +5,5 @@ declare module 'koa' {
     body?: any;
   }
 }
+
+// Koa doesn't define ctx.request.body in its type system, so we need to extend the Request interface to include it. This allows us to access ctx.request.body without TypeScript errors.
